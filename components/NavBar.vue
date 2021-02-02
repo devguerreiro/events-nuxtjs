9eb4e540f01c2ff87fdd9d3882c1749fa6c37e11
Luis Guerreiro <luiscvlh11@gmail.com>
2021-02-02 18:31:14 -0300

    🎨 Add header semantic tag &  mobile attribute navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,77 +1,82 @@
 <template>
-  <nav class="h-12 bg-main-color">
-    <!-- mobile menu -->
-    <div class="md:hidden text-white h-full flex justify-end items-center">
-      <el-dropdown trigger="click">
-        <i class="el-icon-menu mr-4 text-3xl text-blue-400 "/>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item
+  <header>
+    <nav class="h-12 bg-main-color">
+      <!-- mobile menu -->
+      <div
+      class="md:hidden text-white h-full flex justify-end items-center"
+      mobile
+      >
+        <el-dropdown trigger="click">
+          <i class="el-icon-menu mr-4 text-3xl text-blue-400 "/>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item
+            v-for="link in middleLinks"
+            :icon="link.icon"
+            :key="link.label"
+            >
+              <nuxt-link
+              :to="localePath(link.url)"
+              >
+                {{ $t(link.label) }}
+              </nuxt-link>
+            </el-dropdown-item>
+            <el-dropdown-item divided/>
+            <el-dropdown-item
+            v-for="link in rightLinks"
+            :icon="link.icon"
+            :key="link.label"
+            >
+              <nuxt-link
+              :to="localePath(link.url)"
+              >
+                {{ $t(link.label) }}
+              </nuxt-link>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+      <!-- not mobile menu -->
+      <div
+      class="h-full hidden md:flex text-white md:text-lg lg:text-2xl xl:text-base"
+      nav-items
+      >
+        <div
+        class="h-full w-36 mx-4"
+        logo-left
+        >
+          <nuxt-link :to="localePath('index')">
+            <EventLogo />
+          </nuxt-link>
+        </div>
+        <div
+        class="h-full flex flex-1"
+        items-middle
+        >
+          <nuxt-link
           v-for="link in middleLinks"
-          :icon="link.icon"
+          class="flex items-center mx-6 px-2"
           :key="link.label"
+          :to="localePath(link.url)"
           >
-            <nuxt-link
-            :to="localePath(link.url)"
-            >
-              {{ $t(link.label) }}
-            </nuxt-link>
-          </el-dropdown-item>
-          <el-dropdown-item divided/>
-          <el-dropdown-item
+            {{ $t(link.label) }}
+          </nuxt-link>
+        </div>
+        <div
+        class="h-full flex"
+        items-right
+        >
+          <nuxt-link
           v-for="link in rightLinks"
-          :icon="link.icon"
+          class="flex items-center mx-4 px-2"
           :key="link.label"
+          :to="localePath(link.url)"
           >
-            <nuxt-link
-            :to="localePath(link.url)"
-            >
-              {{ $t(link.label) }}
-            </nuxt-link>
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </div>
-    <!-- not mobile menu -->
-    <div
-    class="h-full hidden md:flex text-white md:text-lg lg:text-2xl xl:text-base"
-    nav-items
-    >
-      <div
-      class="h-full w-36 mx-4"
-      logo-left
-      >
-        <nuxt-link :to="localePath('index')">
-          <EventLogo />
-        </nuxt-link>
+            {{ $t(link.label) }}
+          </nuxt-link>
+        </div>
       </div>
-      <div
-      class="h-full flex flex-1"
-      items-middle
-      >
-        <nuxt-link
-        v-for="link in middleLinks"
-        class="flex items-center mx-6 px-2"
-        :key="link.label"
-        :to="localePath(link.url)"
-        >
-          {{ $t(link.label) }}
-        </nuxt-link>
-      </div>
-      <div
-      class="h-full flex"
-      items-right
-      >
-        <nuxt-link
-        v-for="link in rightLinks"
-        class="flex items-center mx-4 px-2"
-        :key="link.label"
-        :to="localePath(link.url)"
-        >
-          {{ $t(link.label) }}
-        </nuxt-link>
-      </div>
-    </div>
-  </nav>
+    </nav>
+  </header>
 </template>
 <script>
 export default {
