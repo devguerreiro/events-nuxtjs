de6fb122c55e7a6d177b7854286c2ec59a10af75
Luis Guerreiro <luiscvlh11@gmail.com>
2021-02-05 12:40:22 -0300

    🎨 Fix input label

--- components/Input.vue
+++ components/Input.vue
@@ -1,31 +1,39 @@
 <template>
   <p class="w-full m-2">
-    <label class="text-white font-bold">{{label}}</label>
+    <label class="text-white font-bold" :for="name">{{ label }}</label>
     <input
-    class="block w-full py-1 px-2 my-1 ring-2 ring-opacity-100 rounded-lg text-black
-    focus:outline-none"
-    :class="invalid ? 'ring-red-400' : 'ring-blue-400'"
-    :type="type"
-    :placeholder="placeholder"
-    :name="name"
-    :required="required"
-    v-model="inputValue"
-    >
+      class="block w-full py-1 px-2 my-1 ring-2 ring-opacity-100 rounded-lg text-black focus:outline-none"
+      :class="invalid ? 'ring-red-400' : 'ring-blue-400'"
+      :id="name"
+      :type="type"
+      :placeholder="placeholder"
+      :name="name"
+      :required="required"
+      v-model="inputValue"
+    />
   </p>
 </template>
 
 <script>
 export default {
-  props: ["value", "type", "placeholder", "name", "label", "required", "invalid"],
+  props: [
+    'value',
+    'type',
+    'placeholder',
+    'name',
+    'label',
+    'required',
+    'invalid',
+  ],
   computed: {
     inputValue: {
       get() {
-        return this.value;
+        return this.value
       },
       set(newValue) {
-        this.$emit("input", newValue)
-      }
-    }
-  }
+        this.$emit('input', newValue)
+      },
+    },
+  },
 }
 </script>
