0493fec07d1d92fcc45d4a2fc6c4a0fa5a48cc73
Luis Guerreiro <luiscvlh11@gmail.com>
2021-02-10 20:05:50 -0300

    ✨ Add new words and phrases to dictionaries

--- lang/en-US.js
+++ lang/en-US.js
@@ -1,20 +1,39 @@
 export default {
+  // nav
   home: 'Home',
   about: 'About',
   contact: 'Contact',
   signIn: 'Sign-in',
   signUp: 'Sign-up',
-  firstName: 'First Name',
-  lastName: 'Last Name',
+  // label
+  username: 'Username',
   email: 'Email',
   password: 'Password',
   passwordConfirmation: 'Confirm your password',
-  typeYourFirstName: 'Type your first name',
-  typeYourLastName: 'Type your last name',
+  city: 'City',
+  state: 'State',
+  // placeholder
+  typeYourUsername: 'Think about some cool username',
   typeYourEmail: 'Type your best email',
   typeYourPassword: 'Type a safe password',
   typeYourPasswordAgain: 'Retype your password',
-  register: "Register",
-  confirm: "Confirm",
-  chooseLanguage: "Choose a language",
+  typeYourCity: 'Where do you live?',
+  typeYourState: 'Where do you from?',
+  // words
+  register: 'Register',
+  send: 'Send',
+  chooseLanguage: 'Choose a language',
+  logo: 'System logo that is a owl wearing a suit',
+  en: 'English',
+  "pt-br": 'Portuguese - Brazil',
+  es: 'Spanish',
+
+  validations: {
+    required: 'This field is required',
+    alpha: 'This field must contain only letters',
+    username: 'This field must contain only letters, numbers and ._',
+    email: 'This field must be a valid email',
+    max: `This field must contain a maximum of {length} characters`,
+    confirmed: '{target} does not match',
+  },
 }
