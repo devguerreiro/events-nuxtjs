64146c83de40e2ea61a79336e3a14a23955e8558
Luis Guerreiro <luiscvlh11@gmail.com>
2021-02-10 21:46:05 -0300

    🚚 Move img directory to inside of assets

--- components/Logo.vue
+++ components/Logo.vue
@@ -2,7 +2,7 @@
   <img
     :title="$t('logo')"
     class="w-full h-full object-contain"
-    src="/img/coruja-logo.png"
+    src="~/assets/img/coruja-logo.png"
     alt="Logo Coruja"
   />
 </template>
